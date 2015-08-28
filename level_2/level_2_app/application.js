70bbac79b627bb9c66fefbf291723e6567ff53d7
Jeff Reiher <jeffreiher@gmail.com>
2015-08-28 11:17:27 -0400

    pseudo class selectors

---
+++ level_2/level_2_app/application.js
@@ -0,0 +1,19 @@
+
+$("#destinations > li");
+$(".promo, #france");
+		//first item in list
+$("#destinations li:first");
+		//last item in list
+$("#destinations li:last");
+		//index starts at 0
+$("#destinations li:odd");
+		//traversing
+$("#destinations").find("li");
+$("li").first();
+$("li").last();
+		//walking the dom method chaining
+$("li").first().next();
+		//walking up the dom
+$("li").first().parent();
+		//up and down the dom
+$("destinations").children("li");
